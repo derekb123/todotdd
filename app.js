9506bd7781e52daab4dbd9c77e29c34251947d9d
derekb123 <derekbutvin@gmail.com>
2021-01-17 19:26:58 -0500

    begin integtration testing and routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const todoRoutes = require('./routes/todo.routes')
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
  res.json('Hello world!');
 });
 
-app.listen(3000, () => {
-  console.log("Server is now running!");
-})
+module.exports = app;
